4ab4f8fa2b19e4665b7858850aeb85bf4d03e216
Jack Willis <jackwillis517@gmail.com>
2023-02-05 13:51:19 -0500

    Updated portfolio link.

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,24 +1,25 @@
-import { ConnectButton } from "web3uikit"
+import { ConnectButton } from "web3uikit";
 export default function Header() {
-    return (
-      <div className="bg-[#222222] grid grid-cols-2 pb-4">
-        <div className="pl-4 text-4xl text-[#F94100] font-turrentroad mt-2"> 
-          <h1>Time Vault</h1>
+  return (
+    <div className="bg-[#222222] grid grid-cols-2 pb-4">
+      <div className="pl-4 text-4xl text-[#F94100] font-turrentroad mt-2">
+        <h1>Time Vault</h1>
+      </div>
+      <div className="flex justify-end">
+        <div>
+          <form action="https://jackwillis.netlify.app/">
+            <button
+              type="submit"
+              className="bg-slate-100 font-bold text-[#FF7324] rounded-2xl px-4 py-2 mt-2 hover:ring-1 hover:ring-[#FF7324] hover:ring-offset-2 hover:text-zinc-500 active:scale-90"
+            >
+              My Portfolio
+            </button>
+          </form>
         </div>
-        <div className="flex justify-end">
-          <div>
-            <form action="https://jackwillis517.github.io/JacksWebsite/">
-              <button 
-                type="submit" 
-                className="bg-slate-100 font-bold text-[#FF7324] rounded-2xl px-4 py-2 mt-2 hover:ring-1 hover:ring-[#FF7324] hover:ring-offset-2 hover:text-zinc-500 active:scale-90">
-                  My Portfolio
-              </button>
-            </form>
-          </div>
-          <div className="mt-2">
-            <ConnectButton moralisAuth={false} className="test"/>
-          </div>
+        <div className="mt-2">
+          <ConnectButton moralisAuth={false} className="test" />
         </div>
       </div>
-    )
+    </div>
+  );
 }
